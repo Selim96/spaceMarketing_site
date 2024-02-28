537fd58a0574a234813e66cd61ed296613e3acd3
Selim <93911103+Selim96@users.noreply.github.com>
2024-02-28 19:19:45 +0200

    add new

--- form.js
+++ form.js
@@ -2,9 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     
     const allLinks = document.querySelectorAll('a');
     const arrOfLinks = [...allLinks];
+
+    const location = window.location.host;
+    console.log(location)
     
     arrOfLinks.forEach(item => {
-        item.href = "spaceMarketing_site#form"
+        item.href = `https://${location}#form`;
     })
     
 
