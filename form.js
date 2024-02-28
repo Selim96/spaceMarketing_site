f46ce033b05e45629a755f98a002c0536c182f8e
Selim <93911103+Selim96@users.noreply.github.com>
2024-02-28 19:23:23 +0200

    add new

--- form.js
+++ form.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const allLinks = document.querySelectorAll('a');
     const arrOfLinks = [...allLinks];
 
-    const location = window.location.host;
+    const location = window.location.pathname;
     console.log(location)
     
     arrOfLinks.forEach(item => {
-        item.href = `https://${location}#form`;
+        item.href = `${location}#form`;
     })
     
 
