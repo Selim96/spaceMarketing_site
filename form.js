c4d3990b8603351452ee2514c72948476173ab50
Selim <93911103+Selim96@users.noreply.github.com>
2024-02-28 18:46:08 +0200

    add

---
+++ form.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const allLinks = document.querySelectorAll('a');
+    const arrOfLinks = [...allLinks];
+    
+    arrOfLinks.forEach(item => {
+        item.href = "/#form"
+    })
+    
+
+});
